992ef9f9a463b6127672f0616f8dfd7bc19f771d
Justyn Hunter <cjhunter@me.com>
2025-12-09 22:42:04 -0600

    day 1 part 1

--- lib/advent_of_code/solution/year_2025/day_01.ex
+++ lib/advent_of_code/solution/year_2025/day_01.ex
@@ -1,6 +1,29 @@
 defmodule AdventOfCode.Solution.Year2025.Day01 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split()
+    |> Enum.map(fn
+      "L" <> n -> -String.to_integer(n)
+      "R" <> n -> String.to_integer(n)
+    end)
+    |> Enum.reduce(%{position: 50, zero_visits: 0}, &rotate/2)
   end
+
+  defp rotate(rotation, %{position: position, zero_visits: zero_visits}) do
+    %{
+      position: Integer.mod(position + rotation, 100),
+      zero_visits:
+        case Integer.mod(position + rotation, 100) do
+          0 -> zero_visits + 1
+          _ -> zero_visits
+        end
+    }
+  end
+
+  def part1(rotations), do: rotations.zero_visits
 
   def part2(_input) do
   end
